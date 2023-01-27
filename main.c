e4c867145692f77c0995d7caaa5a88f72ca7e1b6
Niki NIKOLOV <ngenadie@e2r8p1.clusters.42paris.fr>
2023-01-27 21:17:51 +0100

    ajouter is satty

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ngenadie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 23:45:06 by ngenadie          #+#    #+#             */
-/*   Updated: 2023/01/26 23:45:08 by ngenadie         ###   ########.fr       */
+/*   Updated: 2023/01/27 21:17:23 by ngenadie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int	main(int ac, char **av, char **envp)
 	t_dynarray	darr;
 	int			ret;
 
-	av = NULL;
-	ac = 0;
+	if (isatty(STDIN_FILENO) == 0)
+		return 0;
+	(void)av;
+	(void)ac;
 	if (init_dyn_env(envp, &darr))
 		return (-1);
 	g_vrac.status = 0;
